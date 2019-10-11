99d90a91d463c250ab32e2dc6289693a8a3ce59b
LinXunFeng <598600855@qq.com>
2019-10-11 21:22:54 +0800

    压缩图片

--- gulp_tools/gulpfile.js
+++ gulp_tools/gulpfile.js
@@ -5,6 +5,7 @@ const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 const cssmin = require('gulp-cssmin');
+const imagemin = require('gulp-imagemin');
 
 // 压缩 src/js 目录下的所有js文件，包括子目录
 gulp.task('js_minify', ()=>{
@@ -67,8 +68,20 @@ gulp.task('style', ()=>{
         .pipe(gulp.dest('./build/css/'));
 });
 
+gulp.task('image', ()=>{
+    return gulp
+        .src(['./src/image/**/*'])
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}), // gif隔行扫描
+            imagemin.jpegtran({progressive: true}), // 渐进式
+            imagemin.optipng({optimizationLevel: 5}) // 压缩级别 5：最高级
+        ]))
+        .pipe(gulp.dest('./build/image/'));
+});
+
 // 终端执行gulp后的默认操作
 // gulp.task('default', ['js_minify']);
-gulp.task('default', ['style']);
+// gulp.task('default', ['style']);
+gulp.task('default', ['image']);
 
 // cnpm i gulp-babel @babel/core @babel/preset-env -D
